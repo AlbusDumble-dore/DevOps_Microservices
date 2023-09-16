b82b4cb6558cb3e5e1d4aed91493f1b4e46a9a05
EC2 Default User <ec2-user@ip-172-31-25-89.ec2.internal>
2023-09-16 12:24:49 +0000

    Proj4Part1

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=mukulkmr/proj4
 
 # Step 2:  
 # Authenticate & tag
+docker tag proj4:latest mukulkmr/project4:latest
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push mukulkmr/project4:latest
